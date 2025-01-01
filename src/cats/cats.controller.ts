8dc70056ed2e1a01f745fa40e0cdd3883768bd6b
rodrigosuelli <rodrigosuelli@gmail.com>
2025-01-01 12:39:36 -0300

    Add exception handling and custom filters in CatsController for improved error management

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,19 +1,42 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  BadRequestException,
+  UseFilters,
+} from '@nestjs/common';
 import { CreateCatDto } from './dto/create-cat.dto';
 import { CatsService } from './cats.service';
 import { Cat } from './interfaces/cat.interface';
+import { ForbiddenException } from 'src/forbidden.exception';
+import { HttpExceptionFilter } from './http-exception.filter';
 
 @Controller('cats')
+@UseFilters(HttpExceptionFilter)
 export class CatsController {
   constructor(private catsService: CatsService) {}
 
   @Post()
   async create(@Body() createCatDto: CreateCatDto) {
-    this.catsService.create(createCatDto);
+    try {
+      this.catsService.create(createCatDto);
+      throw new Error('Test error');
+    } catch (error) {
+      throw new BadRequestException('Something bad happened', {
+        cause: error,
+        description: error.message,
+      });
+    }
   }
 
   @Get()
   async findAll(): Promise<Cat[]> {
-    return this.catsService.findAll();
+    try {
+      this.catsService.findAll();
+      throw new BadRequestException();
+    } catch (error) {
+      throw new ForbiddenException();
+    }
   }
 }
